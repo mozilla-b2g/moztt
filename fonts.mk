b1b9aa1d9ba555a6ce75eeb9f05ccc698749cdff
Kai-Zhen Li <kli@mozilla.com>
2014-04-21 21:02:02 +0800

    Bug 992150 - Copy MOZ_PRODUCT_LOCALES related fonts only.

--- fonts.mk
+++ fonts.mk
@@ -23,6 +23,7 @@ else
 TTF_EXT := .ttf
 endif
 
+ifeq ($(strip $(MOZ_PRODUCT_LOCALES)),)
 # Android fonts
 PRODUCT_COPY_FILES += \
     external/moztt/AndroidFonts/DroidNaskh-Regular$(TTF_EXT):system/fonts/DroidNaskh-Regular$(TTF_EXT) \
@@ -95,3 +96,114 @@ PRODUCT_COPY_FILES += \
 #    external/moztt/Lohit/lohit-nepali-ttf-2.5.3/Lohit-Nepali.ttf:system/fonts/Lohit-Nepali.ttf \
 #    external/moztt/Lohit/lohit-tamil-classical-ttf-2.5.3/Lohit-Tamil-Classical.ttf:system/fonts/Lohit-Tamil-Classical.ttf \
 # These may be moved into the build later if localizers explicitly request them via CSS styling.
+else
+# Android fonts
+PRODUCT_COPY_FILES += \
+    external/moztt/AndroidFonts/DroidSerif-Regular$(TTF_EXT):system/fonts/DroidSerif-Regular$(TTF_EXT) \
+    external/moztt/AndroidFonts/DroidSerif-Bold$(TTF_EXT):system/fonts/DroidSerif-Bold$(TTF_EXT) \
+    external/moztt/AndroidFonts/DroidSerif-Italic$(TTF_EXT):system/fonts/DroidSerif-Italic$(TTF_EXT) \
+    external/moztt/AndroidFonts/DroidSerif-BoldItalic$(TTF_EXT):system/fonts/DroidSerif-BoldItalic$(TTF_EXT) \
+    $(NULL)
+
+# MozTT fonts that we don't compress, because there's relatively little gain to be had
+# and/or because they're primary UI fonts that we want instantly available.
+PRODUCT_COPY_FILES += \
+    external/moztt/FiraMono-2.001/FiraMonoOT-Bold.otf:system/fonts/FiraMonoOT-Bold.otf \
+    external/moztt/FiraMono-2.001/FiraMonoOT-Regular.otf:system/fonts/FiraMonoOT-Regular.otf \
+    external/moztt/FiraSans-2.001/FiraSansOT-Bold.otf:system/fonts/FiraSansOT-Bold.otf \
+    external/moztt/FiraSans-2.001/FiraSansOT-BoldItalic.otf:system/fonts/FiraSansOT-BoldItalic.otf \
+    external/moztt/FiraSans-2.001/FiraSansOT-Light.otf:system/fonts/FiraSansOT-Light.otf \
+    external/moztt/FiraSans-2.001/FiraSansOT-LightItalic.otf:system/fonts/FiraSansOT-LightItalic.otf \
+    external/moztt/FiraSans-2.001/FiraSansOT-Medium.otf:system/fonts/FiraSansOT-Medium.otf \
+    external/moztt/FiraSans-2.001/FiraSansOT-MediumItalic.otf:system/fonts/FiraSansOT-MediumItalic.otf \
+    external/moztt/FiraSans-2.001/FiraSansOT-Regular.otf:system/fonts/FiraSansOT-Regular.otf \
+    external/moztt/FiraSans-2.001/FiraSansOT-RegularItalic.otf:system/fonts/FiraSansOT-RegularItalic.otf \
+    $(NULL)
+
+# MozTT fonts that we ship in .woff form when ROM size is at a premium.
+PRODUCT_COPY_FILES += \
+    external/moztt/CharisSILCompact-4.114/CharisSILCompact-B$(TTF_EXT):system/fonts/CharisSILCompact-B$(TTF_EXT) \
+    external/moztt/CharisSILCompact-4.114/CharisSILCompact-BI$(TTF_EXT):system/fonts/CharisSILCompact-BI$(TTF_EXT) \
+    external/moztt/CharisSILCompact-4.114/CharisSILCompact-I$(TTF_EXT):system/fonts/CharisSILCompact-I$(TTF_EXT) \
+    external/moztt/CharisSILCompact-4.114/CharisSILCompact-R$(TTF_EXT):system/fonts/CharisSILCompact-R$(TTF_EXT) \
+    $(NULL)
+
+ifneq ($(findstring ar, $(MOZ_PRODUCT_LOCALES)),) # Arabic, ar
+PRODUCT_COPY_FILES += \
+    external/moztt/AndroidFonts/DroidNaskh-Regular$(TTF_EXT):system/fonts/DroidNaskh-Regular$(TTF_EXT) \
+    $(NULL)
+endif
+
+ifneq ($(findstring bn, $(MOZ_PRODUCT_LOCALES)),) # Bengali, bn-BD
+PRODUCT_COPY_FILES += \
+    external/moztt/Noto/NotoSansBengali-Bold$(TTF_EXT):system/fonts/NotoSansBengali-Bold$(TTF_EXT) \
+    external/moztt/Noto/NotoSansBengali-Regular$(TTF_EXT):system/fonts/NotoSansBengali-Regular$(TTF_EXT) \
+    external/moztt/Noto/NotoSansDevanagari-Bold$(TTF_EXT):system/fonts/NotoSansDevanagari-Bold$(TTF_EXT) \
+    external/moztt/Noto/NotoSansDevanagari-Regular$(TTF_EXT):system/fonts/NotoSansDevanagari-Regular$(TTF_EXT) \
+    external/moztt/Noto/NotoSansTamil-Bold$(TTF_EXT):system/fonts/NotoSansTamil-Bold$(TTF_EXT) \
+    external/moztt/Noto/NotoSansTamil-Regular$(TTF_EXT):system/fonts/NotoSansTamil-Regular$(TTF_EXT) \
+    $(NULL)
+endif
+
+ifneq ($(findstring gez, $(MOZ_PRODUCT_LOCALES)),) # Ge'ez, gez
+PRODUCT_COPY_FILES += \
+    external/moztt/AndroidFonts/DroidSansEthiopic-Regular$(TTF_EXT):system/fonts/DroidSansEthiopic-Regular$(TTF_EXT) \
+    $(NULL)
+endif
+
+ifneq ($(findstring he, $(MOZ_PRODUCT_LOCALES)),) # Hebrew, he
+PRODUCT_COPY_FILES += \
+    external/moztt/AndroidFonts/DroidSansHebrew-Regular$(TTF_EXT):system/fonts/DroidSansHebrew-Regular$(TTF_EXT) \
+    external/moztt/AndroidFonts/DroidSansHebrew-Bold$(TTF_EXT):system/fonts/DroidSansHebrew-Bold$(TTF_EXT) \
+    $(NULL)
+endif
+
+ifneq ($(findstring hi, $(MOZ_PRODUCT_LOCALES)),) # Indian, hi-IN
+PRODUCT_COPY_FILES += \
+    external/moztt/AndroidFonts/DroidNaskh-Regular$(TTF_EXT):system/fonts/DroidNaskh-Regular$(TTF_EXT) \
+    external/moztt/Lohit/lohit-gujarati-ttf-2.5.3/Lohit-Gujarati$(TTF_EXT):system/fonts/Lohit-Gujarati$(TTF_EXT) \
+    external/moztt/Lohit/lohit-kannada-ttf-2.5.3/Lohit-Kannada$(TTF_EXT):system/fonts/Lohit-Kannada$(TTF_EXT) \
+    external/moztt/Lohit/lohit-malayalam-ttf-2.5.3/Lohit-Malayalam$(TTF_EXT):system/fonts/Lohit-Malayalam$(TTF_EXT) \
+    external/moztt/Lohit/lohit-oriya-ttf-2.5.4.1/Lohit-Oriya$(TTF_EXT):system/fonts/Lohit-Oriya$(TTF_EXT) \
+    external/moztt/Lohit/lohit-punjabi-ttf-2.5.3/Lohit-Punjabi$(TTF_EXT):system/fonts/Lohit-Punjabi$(TTF_EXT) \
+    external/moztt/Lohit/lohit-telugu-ttf-2.5.3/Lohit-Telugu$(TTF_EXT):system/fonts/Lohit-Telugu$(TTF_EXT) \
+    external/moztt/Noto/NotoSansBengali-Bold$(TTF_EXT):system/fonts/NotoSansBengali-Bold$(TTF_EXT) \
+    external/moztt/Noto/NotoSansBengali-Regular$(TTF_EXT):system/fonts/NotoSansBengali-Regular$(TTF_EXT) \
+    external/moztt/Noto/NotoSansDevanagari-Bold$(TTF_EXT):system/fonts/NotoSansDevanagari-Bold$(TTF_EXT) \
+    external/moztt/Noto/NotoSansDevanagari-Regular$(TTF_EXT):system/fonts/NotoSansDevanagari-Regular$(TTF_EXT) \
+    external/moztt/Noto/NotoSansTamil-Bold$(TTF_EXT):system/fonts/NotoSansTamil-Bold$(TTF_EXT) \
+    external/moztt/Noto/NotoSansTamil-Regular$(TTF_EXT):system/fonts/NotoSansTamil-Regular$(TTF_EXT) \
+    $(NULL)
+endif
+
+ifneq ($(findstring jp, $(MOZ_PRODUCT_LOCALES)),) # Japanese, jp-JP
+PRODUCT_COPY_FILES += \
+    external/moztt/AndroidFonts/MTLmr3m$(TTF_EXT):system/fonts/MTLmr3m$(TTF_EXT) \
+    external/moztt/AndroidFonts/MTLc3m$(TTF_EXT):system/fonts/MTLc3m$(TTF_EXT) \
+    $(NULL)
+endif
+
+ifneq ($(findstring my, $(MOZ_PRODUCT_LOCALES)),) # Myanmar, my-MM
+PRODUCT_COPY_FILES += \
+    external/moztt/Padauk-2.80/Padauk$(TTF_EXT):system/fonts/Padauk$(TTF_EXT) \
+    external/moztt/Padauk-2.80/Padauk-bold$(TTF_EXT):system/fonts/Padauk-bold$(TTF_EXT) \
+    $(NULL)
+endif
+
+ifneq ($(findstring th, $(MOZ_PRODUCT_LOCALES)),) # Thailand, th
+PRODUCT_COPY_FILES += \
+    external/moztt/DroidSans/DroidSansThai$(TTF_EXT):system/fonts/DroidSansThai$(TTF_EXT) \
+    $(NULL)
+endif
+
+ifneq ($(findstring zh, $(MOZ_PRODUCT_LOCALES)),) # Chinese, zh-CN, zh-TW
+PRODUCT_COPY_FILES += \
+    external/moztt/AndroidFonts/DroidSansFallback$(TTF_EXT):system/fonts/DroidSansFallback$(TTF_EXT) \
+    external/moztt/Roboto-20120823/Roboto-Bold.ttf:system/fonts/Roboto-Bold.ttf \
+    external/moztt/Roboto-20120823/Roboto-BoldItalic.ttf:system/fonts/Roboto-BoldItalic.ttf \
+    external/moztt/Roboto-20120823/Roboto-Italic.ttf:system/fonts/Roboto-Italic.ttf \
+    external/moztt/Roboto-20120823/Roboto-Regular.ttf:system/fonts/Roboto-Regular.ttf \
+    $(NULL)
+endif
+
+endif
